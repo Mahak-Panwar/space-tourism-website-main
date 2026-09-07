6396e1fa5322b133dc9efbb80692e00ebdf47193
Mahak-Panwar <mahakpanwar53@gmail.com>
2026-09-07 17:27:03 +0530

    Fix the error of background images

--- src/utils/Backgroundchange.js
+++ src/utils/Backgroundchange.js
@@ -1,23 +1,23 @@
 // utils/backgrounds.js
 export const backgrounds = {
   home: {
-    desktop: "url('/public/assets/home/bg-home-desktop.jpg')",
-    mobile: "url('/public/assets/home/bg-home-mobile.jpg')",
-    tablet: "url('/public/assets/home/bg-home-tablet.jpg')",
+    desktop: "url('/assets/home/bg-home-desktop.jpg')",
+    mobile: "url('/assets/home/bg-home-mobile.jpg')",
+    tablet: "url('/assets/home/bg-home-tablet.jpg')",
   },
   destination: {
-        desktop: "url('/public/assets/destination/bg-destination-desktop.jpg')",
-    mobile: "url('/public/assets/destination/bg-destination-mobile.jpg')",
-    tablet: "url('/public/assets/destination/bg-destination-desktop.jpg')",
+        desktop: "url('/assets/destination/bg-destination-desktop.jpg')",
+    mobile: "url('/assets/destination/bg-destination-mobile.jpg')",
+    tablet: "url('/assets/destination/bg-destination-desktop.jpg')",
   },
   crew: {
-        desktop: "url('/public/assets/crew/bg-crew-desktop.jpg')",
-    mobile: "url('/public/assets/crew/bg-crew-mobile.jpg')",
-    tablet: "url('/public/assets/crew/bg-crew-desktop.jpg')",
+        desktop: "url('/assets/crew/bg-crew-desktop.jpg')",
+    mobile: "url('/assets/crew/bg-crew-mobile.jpg')",
+    tablet: "url('/assets/crew/bg-crew-desktop.jpg')",
   },
   technology:{
-        desktop: "url('/public/assets/technology/bg-technology-desktop.jpg')",
-    mobile: "url('/public/assets/technology/bg-technology-mobile.jpg')",
-    tablet: "url('/public/assets/technology/bg-technology-desktop.jpg')",
+        desktop: "url('/assets/technology/bg-technology-desktop.jpg')",
+    mobile: "url('/assets/technology/bg-technology-mobile.jpg')",
+    tablet: "url('/assets/technology/bg-technology-desktop.jpg')",
   }
 };
